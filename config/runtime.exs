ea8ce44e671fd4b2f0711ed0e49b486c935a2e3f
Joseph Liu <froseph@gmail.com>
2021-10-10 17:57:19 -0700

    Add npm install in setup

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,7 +48,7 @@ if config_env() == :prod do
   # If you are doing OTP releases, you need to instruct Phoenix
   # to start each relevant endpoint:
   #
-  #     config :sudoku, SudokuWeb.Endpoint, server: true
+  config :sudoku, SudokuWeb.Endpoint, server: true
   #
   # Then you can assemble a release by calling `mix release`.
   # See `mix help release` for more information.
